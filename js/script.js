3cd224478d3479780e9b3feb6a9f3443b5e5762e
jeanramalho <jeanramalho.dev@gmail.com>
2022-07-01 16:57:35 -0300

    Cria logica para criar tabela em banco de dados

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 var db = openDatabase('dbTeste', '1.0', 'Banco de Dados Teste', 2 * 1024 + 1024)
 
+db.transaction(function(tx){
+    tx.executeSql('CREATE TABLE teste (ID PRIMARY KEY, name TEXT)')
+})
