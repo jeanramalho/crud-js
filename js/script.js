85ca6a8a1d709d175878c88b385eb1504042ade8
jeanramalho <jeanramalho.dev@gmail.com>
2022-07-04 14:51:38 -0300

    Insere metodo de salvamento no banco de dados

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
+window.addEventListener('load', carregado)
+
 var db = openDatabase('dbTeste', '1.0', 'Banco de Dados Teste', 2 * 1024 + 1024)
 
 db.transaction(function(tx){
-    tx.executeSql('CREATE TABLE teste (ID PRIMARY KEY, name TEXT)')
+    tx.executeSql('CREATE TABLE nomes (ID PRIMARY KEY, nome TEXT, sobrenome TEXT)')
 })
+
+function carregado() {
+    document.getElementById('salvar').addEventListener('click', salvar)
+}
+
+function salvar() {
+   var nome = document.getElementById('nome').value
+   var sobrenome = document.getElementById('sobrenome').value
+
+   db.transaction(function(tx){
+        tx.executeSql('INSERT INTO nomes (nome, sobrenome) VALUES (?, ?)', [nome,sobrenome])
+   })
+}
