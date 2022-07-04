c9549242e3d90dd41a7439ad77db00e3f305d3c2
jeanramalho <jeanramalho.dev@gmail.com>
2022-07-04 15:39:08 -0300

    Implementa metodo POST

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ window.addEventListener('load', carregado)
 var db = openDatabase('dbTeste', '1.0', 'Banco de Dados Teste', 2 * 1024 + 1024)
 
 db.transaction(function(tx){
-    tx.executeSql('CREATE TABLE nomes (ID PRIMARY KEY, nome TEXT, sobrenome TEXT)')
+    tx.executeSql('CREATE TABLE nomes ( ID INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT)')
 })
 
 function carregado() {
     document.getElementById('salvar').addEventListener('click', salvar)
+    mostrar()
 }
 
 function salvar() {
@@ -17,4 +18,25 @@ function salvar() {
    db.transaction(function(tx){
         tx.executeSql('INSERT INTO nomes (nome, sobrenome) VALUES (?, ?)', [nome,sobrenome])
    })
+
+   mostrar()
+}
+
+function mostrar() {
+    var tabela = document.getElementById('reg')
+
+    db.transaction(function(tx){
+        tx.executeSql('SELECT * FROM nomes', [], function(tx, resultado){
+            var rows = resultado.rows
+            var tr = ''
+            for(let i = 0; i < rows.length; i++) {
+                tr += `<tr>
+                    <td>${rows[i].nome}</td>
+                    <td>${rows[i].sobrenome}</td>
+                </tr>`
+            }
+
+            tabela.innerHTML = tr
+        })
+    }, null)
 }
