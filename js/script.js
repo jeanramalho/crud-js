12b1e262a63a5f57fa1d625e5b78ca32569e7a19
jeanramalho <jeanramalho.dev@gmail.com>
2022-07-05 13:19:56 -0300

    Comenta o código e inicia implementação do estilo do código

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
+//adiciona evento para que quando a página for carregada chamar a função carregado
 window.addEventListener('load', carregado)
 
+//insere a configuração do banco de dados em uma variável
 var db = openDatabase('dbTeste', '1.0', 'Banco de Dados Teste', 2 * 1024 + 1024)
 
+//O db.transaction sempre tem que ser executado para chamar um comando SQL
+//cria uma tabela com os devidos campos
 db.transaction(function(tx){
     tx.executeSql('CREATE TABLE nomes ( ID INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT)')
 })
 
+//função carregado, que é chamada no carregamento da página
+//atribui a função salvar ao botão de salvar
+//chama a função mostrar, para que seja sempre atualizado a exibição ao carregar a página
 function carregado() {
     document.getElementById('salvar').addEventListener('click', salvar)
     mostrar()
 }
 
+//função que salva ou atualiza as informações no banco de dados
+//chama a função mostrar para que seja atualizada a exibição da tabela assim que forem salvas novas informações
+//chama a função limpa para que sejam limpos os campos onde foram digitados as informações
 function salvar() {
 
    var ID = document.getElementById('field-id').value
@@ -29,6 +39,8 @@ function salvar() {
    limpa()
 }
 
+
+//realiza um select no banco de dados para a exibição do conteudo na tela
 function mostrar() {
     var tabela = document.getElementById('reg')
 
@@ -48,6 +60,7 @@ function mostrar() {
     }, null)
 }
 
+//pega o id do item selecionado exibi os dados e os atualiza
 function atualizar(_id){
     console.log(_id)
     var ID = document.getElementById('field-id')
@@ -68,6 +81,7 @@ function atualizar(_id){
     })
 }
 
+//realiza o delete da linha selecionada
 function deletar(){
     var id = document.getElementById('field-id').value
     db.transaction(function(tx){
@@ -77,6 +91,7 @@ function deletar(){
     limpa()
 }
 
+//limpa os campos onde foram inseridos as informações
 function limpa() {
     var nome = document.getElementById('nome')
     var sobrenome = document.getElementById('sobrenome')
